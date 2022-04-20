54da185e8fe83329549a6b9b28790056d588ed47
Tono <tono@pop-os.localdomain>
2022-04-20 00:39:01 -0400

    cleaned repo

--- Attributes/RequiredForCreate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ublux.Communications.Attributes;
-
-/// <summary>
-///     Properties containing this attribute will not be shown on swagger documentation.
-///     For example when creating a new Phone we do not want to see the property DateCreated
-/// </summary>
-public class HideForCreateRequestAttribute : Attribute
-{
-}
