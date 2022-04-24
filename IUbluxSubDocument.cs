4d407df455dd7ee0727b0f370568ebb3a1184075
Antonio <antonio@ublux.com>
2022-04-24 19:01:47 -0400

    saved changes

--- IUbluxSubDocument.cs
+++ IUbluxSubDocument.cs
@@ -4,7 +4,7 @@
 ///     All objects stored on database must contain this properties
 ///     T must represent it's mapping DTO
 /// </summary>
-public abstract class UbluxSubDocument : IUbluxSubDocument
+public abstract class UbluxSubDocument //: IUbluxSubDocument
 {
     // Todo mark this property on database with unique index
 
@@ -25,29 +25,29 @@ public abstract class UbluxSubDocument : IUbluxSubDocument
 
 }
 
-/// <summary>
-///     All collections on database must contain this attribute.
-///     Represents a Table on a traditional database
-/// </summary>
-public interface IUbluxSubDocument 
-{
-    ///// <summary>
-    /////     Session token
-    ///// </summary>
-    //[AllowUpdate(false)]
-    //[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
-    //    //string? _id { get; set; }
+///// <summary>
+/////     All collections on database must contain this attribute.
+/////     Represents a Table on a traditional database
+///// </summary>
+//public interface IUbluxSubDocument 
+//{
+//    ///// <summary>
+//    /////     Session token
+//    ///// </summary>
+//    //[AllowUpdate(false)]
+//    //[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
+//    //    //string? _id { get; set; }
 
 
-    ///// <summary>
-    /////     Prefix of subdocument. For example for a Line it will be Li
-    ///// </summary>
-    //string SubDocumentPrefix();
+//    ///// <summary>
+//    /////     Prefix of subdocument. For example for a Line it will be Li
+//    ///// </summary>
+//    //string SubDocumentPrefix();
 
-    ///// <summary>
-    /////     Set id of subdocument
-    ///// </summary>
-    //void SetId();
+//    ///// <summary>
+//    /////     Set id of subdocument
+//    ///// </summary>
+//    //void SetId();
 
 
-}
+//}
