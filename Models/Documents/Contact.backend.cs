4ec174d1312725624a0f686db2c9b4f3c87b979c
Antonio <antonio@ublux.com>
2022-05-26 18:32:33 -0400

    removed unused contacts counter

--- Models/Documents/Contact.backend.cs
+++ Models/Documents/Contact.backend.cs
@@ -13,29 +13,6 @@ public partial class Contact : UbluxDocument, IReferncesAccount
     [AllowUpdate(false)]
     [IsRequired]
     public string IdAccount { get; set; } = String.Empty;
-
-    /// <summary>
-    ///     Because we sync contacts their ids must be as small as possible.
-    ///     We convert ths counter to base62. For example contact 10,000 will have id Xa for example
-    /// </summary>
-    private static int _idContactGenerationCounter;
-
-    /// <summary>
-    ///     Because we sync contacts their ids must be as small as possible.
-    ///     We convert ths counter to base62. For example contact 10,000 will have id Xa for example
-    /// </summary>
-    public static int GetNextContactGenerationCounter()
-    {
-        return Interlocked.Increment(ref _idContactGenerationCounter);
-    }
-
-    /// <summary>
-    ///     Set only once
-    /// </summary>    
-    public static void SetContactGenerationCounter(int value)
-    {
-        _idContactGenerationCounter = value;
-    }
 }
 
 #endif
