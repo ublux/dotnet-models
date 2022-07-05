a52ada4ff11f683c57ae6b67f11b732cae5ebab4
Antonio <antonio@ublux.com>
2022-07-05 01:39:05 -0400

    added model LineConnectionStatusChanged

---
+++ Models/LineConnectionStatusChanged.backend.cs
@@ -0,0 +1,35 @@
+﻿#if UBLUX_BACKEND
+
+namespace Ublux.Communications.Models;
+
+/// <summary>
+///     Used to sync line connection status changes with WS
+/// </summary>
+public class LineConnectionStatusChanged
+{
+    /// <summary>
+    ///     Id of line that was modified
+    /// </summary>
+    [AllowUpdate(false)]
+    public string IdLine { get; set; } = string.Empty;
+
+    ///// <summary>
+    /////     Id of phone
+    ///// </summary>
+    //[AllowUpdate(false)]
+    //public string IdPhone { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Id of Account
+    /// </summary>
+    [AllowUpdate(false)]
+    public string IdAccount { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Clone this to avoid sync problems.
+    /// </summary>
+    [AllowUpdate(false)]
+    public LineConnectionStatus NewLineConnectionStatus { get; set; } = new ();
+}
+
+#endif
