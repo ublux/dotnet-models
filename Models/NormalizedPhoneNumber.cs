304f0a5a58388017bbd7d4b85e5e5df340cd655f
Antonio <antonio@ublux.com>
2022-08-21 17:58:39 -0400

    added normalized phone number model

---
+++ Models/NormalizedPhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿namespace Ublux.Communications.Models;
+
+/// <summary>
+///     Normalized phone number
+/// </summary>
+public class NormalizedPhoneNumber
+{
+    /// <summary>
+    ///     Phone number in national format. Example +13051234567
+    /// </summary>
+    public string? NationalFormat { get; set; }
+
+    /// <summary>
+    ///     Is it a mobile phone number? Null if unknown
+    /// </summary>
+    public bool? IsMobile { get; set; }
+
+    /// <summary>
+    ///     Country of phone number. Null if unknown
+    /// </summary>
+    public CountryIsoCode? CountryIsoCode { get; set; }
+}
