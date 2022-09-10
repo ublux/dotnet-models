24f473d6e58da7aa710622cfbee5fcee77ceb6c3
Antonio <antonio@ublux.com>
2022-09-10 14:47:29 -0400

    added sns models

---
+++ Enums/SnsTopic.cs
@@ -0,0 +1,22 @@
+﻿namespace Ublux.Communications.Enums;
+
+/// <summary>
+///     Simple notification service topic. Example call_completed.
+/// </summary>
+public enum SnsTopic
+{
+    /// <summary>
+    ///     Item Created
+    /// </summary>
+    Collection_Created,
+
+    /// <summary>
+    ///     Item updated
+    /// </summary>
+    Collection_Updated,
+
+    /// <summary>
+    ///     Item deleted
+    /// </summary>
+    Collection_Deleted,
+}
---
+++ Models/SnsMessage.cs
@@ -0,0 +1,59 @@
+﻿namespace Ublux.Communications.Models;
+
+/// <summary>
+///     Simple notification service message
+/// </summary>
+public class SnsMessage 
+{
+    /// <summary>
+    ///     Id of message
+    /// </summary>
+    [IsRequired]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    /// <summary>
+    ///     Date when message was created in UTC
+    /// </summary>
+    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+    /// <summary>
+    ///     Topic or category type
+    /// </summary>
+    [IsRequired]
+    public required SnsTopic SnsTopic { get; set; }
+
+    /// <summary>
+    ///     Message data
+    /// </summary>
+    public string? Message { get; set; }
+
+    /// <summary>
+    ///     Id of account
+    /// </summary>
+    public string? IdAccount { get; set; }
+
+    /// <summary>
+    ///     If a phone was modified then the id of the phone
+    /// </summary>
+    public string? IdCollection { get; set; }
+
+    /// <summary>
+    ///     Example Accounts
+    /// </summary>
+    public Collections? Collection { get; set; }
+
+    /// <summary>
+    ///     Data of message. This may be json data
+    /// </summary>
+    public object? Data { get; set; }
+
+    /// <summary>
+    ///     If there is data then what type of data is it
+    /// </summary>
+    public string? DataType { get; set; }
+
+    /// <summary>
+    ///     Created by instance id
+    /// </summary>
+    public required string Origin { get; set; }
+}
