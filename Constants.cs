57f37c5b6cc2eb927244eef73a8a33d9fd79d8df
Antonio <antonio@ublux.com>
2022-09-27 01:30:27 -0400

    added regex constant to find ip address since it is very comonly used

--- Constants.cs
+++ Constants.cs
@@ -1,9 +1,11 @@
-﻿namespace Ublux.Communications.Models;
+﻿using System.Text.RegularExpressions;
+
+namespace Ublux.Communications.Models;
 
 /// <summary>
 ///     Application constants
 /// </summary>
-public static class Constants
+public static partial class Constants
 {
     /// <summary>
     ///     Domain for application
@@ -19,4 +21,16 @@ public static class Constants
     ///     Example: https://api.ublux.com
     /// </summary>
     public const string WebService_BaseUrl = $"https://{WebService_HostName}";
+
+    /// <summary>
+    ///     Regex used to find ip address V4 and port
+    /// </summary>
+    [GeneratedRegex("(\\d+\\.\\d+\\.\\d+\\.\\d+):(\\d+)")]
+    public static partial Regex RegexIpAndPort();
+
+    /// <summary>
+    ///     Regex used to find ip address V4
+    /// </summary>
+    [GeneratedRegex("(\\d+\\.\\d+\\.\\d+\\.\\d+)")]
+    public static partial Regex RegexIp();
 }
