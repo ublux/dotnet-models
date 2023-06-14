baf8e4865ff0c265033b1260d4fe600d6f2c880b
Antonio <antonio@ublux.com>
2023-06-14 11:07:21 -0400

    Created method to creaate default call flow logic

--- Models/Documents/CallFlowLogic.backend.cs
+++ Models/Documents/CallFlowLogic.backend.cs
@@ -17,6 +17,32 @@ public partial class CallFlowLogic
         foreach (var node in Tree.TraverseNode())
             yield return node;
     }
+
+    /// <summary>
+    ///     Create default logic
+    /// </summary>
+    public static CallFlowLogic CreateDefault(RunningApplicationInstance instance, string idAccount)
+    {
+        return new CallFlowLogic()
+        {
+            DateCreated = DateTime.UtcNow,
+            BuiltId = CallFlowLogic.BuildId(instance),
+            FriendlyName = "Default",
+            IdAccount = idAccount,
+            IdsTags = new(),
+            DateDeleted = null,
+            Description = "Default IVR",
+            Tree = new NodeSay()
+            {
+                Child = new NodeHangup(),
+                Text = $"This number is not configured. Please configure your new number at {Constants.Domain}",
+                Voice = "Amy",
+                CollectDigits = false,
+                Timeout = 1,
+                MaxNumberOfDigits = 1
+            }
+        };
+    }
 }
 
 #endif
