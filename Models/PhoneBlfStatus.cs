4430561b637c357599a17a84bb5d3e414f535fca
Antonio <antonio@ublux.com>
2023-07-19 22:55:51 -0400

    added IdPhoneConnectedWith and IsIncoming properties to BLF

--- Models/PhoneBlfStatus.cs
+++ Models/PhoneBlfStatus.cs
@@ -28,4 +28,16 @@ public class PhoneBlfStatus
     /// </summary>
     [UbluxValidationRequired]
     public BlfStatusType BlfStatusType { get; set; }
+
+    /// <summary>
+    ///     Is incoming or outgoing channel. If receiving phone call it is incoming. If it is making call it is outgoing.
+    ///     In other words it if true it means it goes from asterisk to phone
+    ///     If false it means it goes from phone to asterisk
+    /// </summary>
+    public required bool IsIncoming { get; set; }
+
+    /// <summary>
+    ///     With what ip phone is connected. Empty string if PSTN
+    /// </summary>
+    public string? IdPhoneConnectedWith { get; set; }
 }
